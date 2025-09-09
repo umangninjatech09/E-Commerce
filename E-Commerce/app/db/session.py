ac8a39709ad8c9ff88ac8ec77b83c34449557645
django views <meshwaninjatech12@gmail.com>
2025-09-09 16:15:52 +0530

    Modify DB

--- E-Commerce/app/db/session.py
+++ E-Commerce/app/db/session.py
@@ -2,8 +2,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-
-
 # Dependency to get DB session
 def get_db():
     db = SessionLocal()
