bc5d5c97e1b779989b187148d612d3b1fecd0666
django views <meshwaninjatech12@gmail.com>
2025-09-12 18:49:06 +0530

    Fix order service, update models and endpoints

--- E-Commerce/app/db/session.py
+++ E-Commerce/app/db/session.py
@@ -1,15 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker 
-
-DATABASE_URL = "sqlite:///./E-Commerce.db"  
-
-Base = declarative_base()
-
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-# Base.metadata.create_all(bind=engine)
+from sqlalchemy.orm import sessionmaker
 
 # Dependency to get DB session
 def get_db():
@@ -19,6 +10,10 @@ def get_db():
     finally:
         db.close()
 
-def init_db():
-    from app.models import Product, Inventory, Pricing, Customer, SearchIndex
-    Base.metadata.create_all(bind=engine)
+DATABASE_URL = "sqlite:///./E-Commerce.db"  
+
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+Base = declarative_base()
--- E-Commerce/app/models/__init__.py
+++
@@ -1,6 +0,0 @@
-# app/models/__init__.py
-from .product import Product
-from .inventory import Inventory
-from .pricing import Pricing
-from .customer import Customer
-from .search import SearchIndex
