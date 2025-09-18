0c9d69985bc3847bc708c588bbd828c30b5a6d8d
Umang <umangninjatech09@gmail.com>
2025-09-18 11:13:48 +0530

    Add pagination

---
+++ .history/E-Commerce/app/api/v1/endpoints/purchase_20250918105820.py
@@ -0,0 +1,43 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+from app.schemas.purchase import Purchase, PurchaseCreate
+from app.crud.purchase import create_purchase, get_all_purchase, get_purchases, update_purchase, delete_purchase
+from app.db.session import get_db
+from typing import List
+from fastapi import Query
+from app.utils.pagination import paginate
+from app.schemas.common import Page
+
+
+router = APIRouter()
+
+
+@router.post("/", response_model=Purchase)
+def create_purchase(purchase: PurchaseCreate, db: Session = Depends(get_db)):
+    return create_purchase(db, purchase)
+
+
+@router.get("/", response_model=Page[schePurchase])
+def list_purchase(
+    db: Session = Depends(get_db),
+    page: int = Query(1, ge=1),
+    size: int = Query(10, ge=1, le=100)
+):
+    query = db.query(Purchase)
+    return paginate(query, page, size)
+
+
+@router.put("/{purchase_id}", response_model=Purchase)
+def update_purchases(purchase_id: int, purchase: PurchaseCreate, db: Session = Depends(get_db)):
+    db_purchase = update_purchase(db, purchase_id, purchase)
+    if not db_purchase:
+        raise HTTPException(status_code=404, detail="Purchase not found")
+    return db_purchase
+
+@router.delete("/{purchase_id}", response_model=Purchase)
+def delete_purchases(purchase_id: int, db: Session = Depends(get_db)):
+    db_purchase = delete_purchase(db, purchase_id)
+    if not db_purchase:
+        raise HTTPException(status_code=404, detail="Purchase not found")
+    return db_purchase
+
